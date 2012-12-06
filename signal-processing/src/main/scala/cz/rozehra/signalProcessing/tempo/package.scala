5670a53cf128b406c3fe9a9f6eb0cd0e5102ac9c
jlibovicky <jlibovicky@gmail.com>
2012-12-06 14:10:58 +0100

    Tempo estimation +/- finished (further debugging necessary, fails on train1.wav)

---
+++ signal-processing/src/main/scala/cz/rozehra/signalProcessing/tempo/package.scala
@@ -0,0 +1,12 @@
+package cz.rozehra.signalProcessing
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jindrich
+ * Date: 12/4/12
+ * Time: 11:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package object tempo {
+
+}
